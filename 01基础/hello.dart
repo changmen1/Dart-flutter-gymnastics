1b5329138107ce13319b4e4ec6b7ed7738dfb4c3
zxl17339811909@163.com <zxl17339811909>
2024-07-30 19:54:17 +0800

    init

---
+++ 01基础/hello.dart
@@ -0,0 +1,55 @@
+void main(List<String> args) {
+  // var str = 'Zxl';
+  // print(str);
+  // var num = 1234;
+  // print(num);
+
+  //dart里面有类型校验
+
+  //字符串
+  // String strr = '你好';
+  // print(strr);
+
+  //数字类型
+  // int numb = 123;
+  // print(numb);
+
+  // String strs = '333';
+  // print(strs);
+
+  //final const 定义 常量不可改变
+  // final str1 = 'final123';
+  // print(str1);
+  // const str2 = 'const123';
+  // print(str2);
+  // strs = 'final456';
+  // print(strs);
+  //当前时间
+  // final nowtime = new DateTime.now();
+  // const nowtime2 = new DateTime.now();
+  // print(nowtime);
+
+  // String str3 = 'str';
+  // String str4 = "str";
+  // String str5 = '''str
+  //   str2
+  //   str3
+  // ''';
+  // print(str3);
+  // print(str4);
+  // print(str5);
+
+  //字符串拼接
+  // String str = 'Zxl';
+  // String str2 = 'Dart';
+  // print("$str $str2");
+  // print(str + " " + str2);
+
+  /** Dart数据类型 数值类型 */
+  // 1.int
+  int a = 123;
+  print(a);
+  // 2.double
+  double b = 23.5;
+  print(b);
+}
